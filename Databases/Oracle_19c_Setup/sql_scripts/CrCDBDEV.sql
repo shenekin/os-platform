c0d720deb2aa2f63612d6038fddfddfdba7b8ed3
shenekin <8631505+ekinshen@user.noreply.gitee.com>
2025-08-27 15:12:22 +0000

    update Databases/Oracle_19c_Setup/sql_scripts/CrCDBDEV.sql. Modify sql create db
    Signed-off-by: shenekin <8631505+ekinshen@user.noreply.gitee.com>

--- Databases/Oracle_19c_Setup/sql_scripts/CrCDBDEV.sql
+++ Databases/Oracle_19c_Setup/sql_scripts/CrCDBDEV.sql
@@ -1,8 +1,19 @@
 CREATE DATABASE cdbdev
-USER SYS IDENTIFIED BY oracle
-USER SYSTEM  IDENTIFIED BY oracle
-EXTENT MANAGEMENT LOCAL
-DEFAULT TEMPORARY TABLESPACE temp
-DEFAULT TABLESPACE users
-UNDO TABLESPACE undotbs1
-ENABLE_PLUGGABLE_DATABASE;
+   USER SYS IDENTIFIED BY "SysPassw0rd"
+   USER SYSTEM IDENTIFIED BY "SysPassw0rd"
+   LOGFILE GROUP 1 ('/u01/app/oracle/oradata/cdbdev/redo01.log') SIZE 200M,
+           GROUP 2 ('/u01/app/oracle/oradata/cdbdev/redo02.log') SIZE 200M,
+           GROUP 3 ('/u01/app/oracle/oradata/cdbdev/redo03.log') SIZE 200M
+   MAXLOGFILES 5
+   MAXDATAFILES 100
+   DATAFILE '/u01/app/oracle/oradata/cdbdev/system01.dbf' SIZE 700M REUSE
+   SYSAUX DATAFILE '/u01/app/oracle/oradata/cdbdev/sysaux01.dbf' SIZE 550M REUSE
+   DEFAULT TEMPORARY TABLESPACE temp
+      TEMPFILE '/u01/app/oracle/oradata/cdbdev/temp01.dbf' SIZE 100M REUSE
+   DEFAULT TABLESPACE users
+      DATAFILE '/u01/app/oracle/oradata/cdbdev/users01.dbf' SIZE 100M REUSE
+   UNDO TABLESPACE undotbs1
+      DATAFILE '/u01/app/oracle/oradata/cdbdev/undotbs01.dbf' SIZE 200M REUSE
+   EXTENT MANAGEMENT LOCAL
+   ENABLE PLUGGABLE DATABASE
+   SEED FILE_NAME_CONVERT = ('/u01/app/oracle/oradata/cdbdev/', '/u01/app/oracle/oradata/cdbdev/pdbseed/');
